24616b1188253b4e892e0d6d4d66d79e7ff0382d
bolojutsu <200mshinobi@gmail.com>
2026-04-23 10:56:10 -0400

    completed Linked List

--- LinkedList.java
+++ LinkedList.java
@@ -1,10 +1,14 @@
+import java.util.Objects;
+import java.util.Vector;
 
 public class LinkedList<E extends KeyedElementInterface<K>, K> implements ListInterface<E, K> {
     private int count;
+    private Node front;
+    private Node back;
 
-    public class Node<E> {
+    public class Node {
         private E element;
-        private Node<E> next;
+        private Node next;
 
         public Node(E element) {
             this.element = element;
@@ -15,7 +19,7 @@ public class LinkedList<E extends KeyedElementInterface<K>, K> implements ListIn
             return element;
         }
 
-        public Node<E> getNext() {
+        public Node getNext() {
             return this.next;
         }
 
@@ -23,14 +27,21 @@ public class LinkedList<E extends KeyedElementInterface<K>, K> implements ListIn
             this.element = element;
         }
 
-        public void setNext(Node<E> next) {
+        public void setNext(Node next) {
             this.next = next;
         }
     }
 
     @Override
     public ElementIterator<E> iterator() {
-        return null;
+        Vector<E> vector = new Vector<>();
+        Node current = this.front;
+
+        while (current != null) {
+            vector.add(current.getElement());
+            current = current.getNext();
+        }
+        return new ElementIterator<>(vector);
     }
 
     @Override
@@ -45,31 +56,112 @@ public class LinkedList<E extends KeyedElementInterface<K>, K> implements ListIn
 
     @Override
     public ListInterface<E, K> copy() {
-        return null;
+        LinkedList<E, K> copiedList = new LinkedList<>();
+        Node current = this.front;
+
+        while (current != null) {
+            E copiedElement = (E) current.getElement().copy();
+            copiedList.add(copiedElement);
+            current = current.getNext();
+        }
+        return copiedList;
     }
 
     @Override
     public E add(E element) {
-        return null;
+        if (element == null) {
+            return null;
+        }
+
+        E displacedElement = this.remove(element.getKey());
+        Node newNode = new Node(element);
+
+        if (this.isEmpty()) {
+            this.front = newNode;
+            this.back = newNode;
+        } else {
+            this.back.setNext(newNode);
+            this.back = newNode;
+        }
+
+        this.count++;
+        return displacedElement;
     }
 
     @Override
     public E get(K key) {
+        Node current = this.front;
+
+        while (current != null) {
+            E currentElement = current.getElement();
+            if (Objects.equals(currentElement.getKey(), key)) {
+                return currentElement;
+            }
+            current = current.getNext();
+        }
+
         return null;
     }
 
     @Override
     public E replace(E element) {
+        if (element == null) {
+            return null;
+        }
+
+        K targetKey = element.getKey();
+        Node current = this.front;
+
+        while (current != null) {
+            E currentElement = current.getElement();
+            if (Objects.equals(currentElement.getKey(), targetKey)) {
+                current.setElement(element);
+                return currentElement;
+            }
+            current = current.getNext();
+        }
+
         return null;
     }
 
     @Override
     public E remove(K key) {
+        Node current = this.front;
+        Node previous = null;
+
+        while (current != null) {
+            E currentElement = current.getElement();
+            if (Objects.equals(currentElement.getKey(), key)) {
+                if (previous == null) {
+                    this.front = current.getNext();
+                } else {
+                    previous.setNext(current.getNext());
+                }
+
+                if (current == this.back) {
+                    this.back = previous;
+                }
+
+                this.count--;
+                if (this.count == 0) {
+                    this.front = null;
+                    this.back = null;
+                }
+
+                return currentElement;
+            }
+
+            previous = current;
+            current = current.getNext();
+        }
+
         return null;
     }
 
     @Override
     public void removeAll() {
-
+        this.front = null;
+        this.back = null;
+        this.count = 0;
     }
 }
