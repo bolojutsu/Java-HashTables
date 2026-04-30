7bdacef77950ea7b2678f5cb2b7d94d1439ca1db
Devin Mateo <200mshinobi@gmail.com>
2026-04-30 06:47:54 -0400

    complete hashtable.java class

--- HashTable.java
+++ HashTable.java
@@ -1,5 +1,6 @@
+import java.util.Vector;
 public class HashTable<E extends KeyedElementInterface<K>, K> implements HashTableInterface<E, K> {
-    private static int NUMBER_OF_BUCKETS = 26;
+    public static int NUMBER_OF_BUCKETS = 26;
     private LinkedList<E, K>[] buckets;
     private int size;
 
@@ -30,13 +31,8 @@ public class HashTable<E extends KeyedElementInterface<K>, K> implements HashTab
         }
     }
 
-    private int hashFunction(int key) {
-        int hashCode = key % buckets.length;
-        return hashCode;
-    }
-
-    private int probe(int hashCode, int i) {
-        return (hashCode + i + (i * i)) % buckets.length;
+    private int hashFunction(K key) {
+        return(key.hashCode()) % buckets.length;
     }
 
     @Override
@@ -52,48 +48,89 @@ public class HashTable<E extends KeyedElementInterface<K>, K> implements HashTab
     @Override
     public HashTableInterface<E, K> copy() {
         HashTable<E, K> copyTable = new HashTable<>();
-
-        return null;
+        for(int i = 0; i < buckets.length; i++) {
+            copyTable.buckets[i] = (LinkedList<E, K>) this.buckets[i].copy();
+        }
+        copyTable.size = this.size;
+        return copyTable;
     }
 
     @Override
     public ElementIterator<E> iterator() {
-        return null;
+        Vector<E> vector = new Vector<>();
+        for(int i = 0; i< buckets.length; i++) {
+            ElementIterator<E> bucketIterator = (ElementIterator<E>) buckets[i].iterator();
+            while(bucketIterator.hasNext()) {
+                vector.add(bucketIterator.next());
+            }
+        }
+        return new ElementIterator<>(vector);
     }
 
     @Override
     public E insert(E element) {
-        return null;
+        if(element == null) {
+            return null;
+        }
+        int index = hashFunction(element.getKey());
+        E displacedElement = buckets[index].add(element);
+
+        if( displacedElement == null) {
+            this.size++;
+        }
+        return displacedElement;
     }
 
     @Override
     public E get(K key) {
-        return null;
+        if(key == null) {
+            return null;
+        }
+        int index = hashFunction(key);
+        return buckets[index].get(key);
     }
 
     @Override
     public E remove(K key) {
-        return null;
+        if(key == null) {
+            return null;
+        }
+        int index = hashFunction(key);
+        E removedElement = buckets[index].remove(key);
+
+        if(removedElement != null) {
+            this.size--;
+        }
+        return removedElement;
     }
 
     @Override
     public void removeAll() {
-
+        for(int i = 0; i < buckets.length; i++) {
+            buckets[i].removeAll();
+        }
+        this.size = 0;
     }
 
     @Override
     public int getSizeOfLargestBucket() {
-        return 0;
+        int largest = 0;
+        for(int i = 0; i< buckets.length; i++){
+            if(buckets[i].size() > largest) {
+                largest = buckets[i].size();
+            }
+        }
+        return largest;
     }
 
     @Override
     public double getAverageBucketSize() {
-        return 0.0;
+        return (double) this.size / NUMBER_OF_BUCKETS;
     }
 
     @Override
     public Object[] getBuckets() {
-        return null;
+        return this.buckets;
     }
 
 }
