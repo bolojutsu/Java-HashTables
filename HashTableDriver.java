b67ff3c1022ba8a43d9fae54edadc9851e02de56
Devin Mateo <200mshinobi@gmail.com>
2026-05-03 13:27:20 -0400

    Driver class

--- HashTableDriver.java
+++ HashTableDriver.java
@@ -1,15 +1,30 @@
 public class HashTableDriver implements HashTableDriverInterface {
 
     public Person<Integer>[] createPersons(int numPeople) {
-        return null;
+        Person<Integer>[] people = new Person[numPeople];
+        for(int i = 0; i < numPeople; i++) {
+            people[i] = new Person<>(i);
+        }
+        return people;
     }
 
     public LinkedList<Person<Integer>, Integer>[] createBuckets(int numBuckets) {
-        return null;
+        LinkedList<Person<Integer>, Integer>[] buckets = new LinkedList[numBuckets];
+        for(int i = 0; i < numBuckets; i++) {
+            buckets[i] = new LinkedList<>();
+        }
+        return buckets;
     }
 
     public HashTable<Person<Integer>, Integer> createHashTable(TestType testType, int numPeople) {
-        return null;
+        HashTable<Person<Integer>, Integer> hashTable = new HashTable<>();
+        if(testType == TestType.Get || testType == TestType.Remove) {
+            Person<Integer>[] people = createPersons(numPeople);
+            for(int i = 0; i < numPeople; i++) {
+                hashTable.insert(people[i]);
+            }
+        }
+        return hashTable;
     }
 
     public Object[] runTestCase(TestType testType, int numPeople, int numberOfTimes) {
